fe98cd4bb900ecfd8dde00c993346c8b53f2b72e
mar-tina <martinakhaemba@gmail.com>
2020-02-13 22:40:21 +0300

    Tutorial setup

--- example/todo-app/src/components/todo.js
+++ example/todo-app/src/components/todo.js
@@ -1,3 +1,27 @@
 import {
-  html
+  UNF,
+  html,
+  UNFBaseElement
 } from '../../../../src'
+
+let localState = {}
+
+function TodoApp() {
+  this.lifecyle.onMount = (ctx) => {
+    localState = ctx;
+    console.log("The local state", localState);
+    setExternalState(ctx)
+    console.log("Outside state", ctx.state)
+  }
+}
+
+function setExternalState(ctx) {
+  localState = ctx
+  console.log("LOCAL STATE", localState)
+
+}
+
+
+TodoApp.call(UNFBaseElement)
+
+UNF.Base.render("todo-app", UNFBaseElement.ci);
