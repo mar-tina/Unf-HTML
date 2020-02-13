64086169851a3973964d9e33b0f663ff68ea8143
mar-tina <martinakhaemba@gmail.com>
2020-02-13 19:33:18 +0300

    minor fix

--- example/todo-app/src/components/test.js
+++ example/todo-app/src/components/test.js
@@ -120,26 +120,28 @@ let addListenerForAllTodos = function (ctx, docToRerender) {
 let todoList = (todos) => {
   if (isArrayEmpty(todos) || todos == undefined) {
     return `<div> TodoList is empty </div>`
+  } else {
+    return (
+      html `
+          ${todos.map(x => (
+            `<div>
+              <p style"${todoItem}"> ${x.id} ${x.label} ${x.done}
+              <span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="${
+                  x.done ? `green` : `grey`
+                }" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"></polyline></svg>
+              </span>
+              <span id="${x.id}">
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="red" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
+              </span>
+              </p>
+            </div>`
+          ))}
+        `
+    )
   }
 
-  return (
-    html `
-        ${todos.map(x => (
-          `<div>
-            <p style"${todoItem}"> ${x.id} ${x.label} ${x.done}
-            <span>
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="${
-                x.done ? `green` : `grey`
-              }" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"></polyline></svg>
-            </span>
-            <span id="${x.id}">
-              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="red" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
-            </span>
-            </p>
-          </div>`
-        ))}
-      `
-  )
+
 }
 
 let testTemplate = html `
