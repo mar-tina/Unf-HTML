84b6775a845043e08fb6a10d0eb35dc877a2e849
mar-tina <martinakhaemba@gmail.com>
2020-02-15 17:54:07 +0300

    ui fix

--- example/todo-app/src/components/todo.js
+++ example/todo-app/src/components/todo.js
@@ -107,11 +107,7 @@ let todoList = (todos) => {
     return html `
       ${todos.map(x => (
         `<p style"${todoItem}"> ${x.label} ${x.done}
-        <span>
-          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="${
-            x.done ? `green` : `grey`
-          }" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"></polyline></svg>
-        </span>
+      
         <span id="${x.id}">
           <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="red" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg>
         </span>
