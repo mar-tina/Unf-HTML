4f67ffd906ebaa8972aa7c8b64b801441d04a8c4
mar-tina <martinakhaemba@gmail.com>
2020-02-15 13:19:45 +0300

    minor app adjustments

--- example/todo-app/src/components/todo.js
+++ example/todo-app/src/components/todo.js
@@ -1,27 +1,64 @@
 import {
   UNF,
   html,
-  UNFBaseElement
+  unfBaseElement
 } from '../../../../src'
 
-let localState = {}
 
-function TodoApp() {
+async function TodoApp() {
   this.lifecyle.onMount = (ctx) => {
-    localState = ctx;
-    console.log("The local state", localState);
-    setExternalState(ctx)
-    console.log("Outside state", ctx.state)
+    console.log("I have the context", ctx);
+  }
+
+  this.proxies = {
+    onFoodChanged: (ctx, elID) => {
+      console.log("Running food change proxy");
+      let todos = ctx.state["todos"]
+
+      let f = () => {
+        UNF.Events.bindRenderToStateChange(ctx, elID, todoList, todos);
+      }
+
+      let foodProxy = ctx.state.watcher(f, {
+        todos: todos
+      })
+
+      return foodProxy
+    }
+  }
+
+  this.methods = {
+    handleBtnClick: (ctx) => {
+      let changeElButton = UNF.Base.getElement(ctx, "change-state");
+      UNF.Base.addListener("onclick", changeElButton, (e) => {
+        ctx.state.food = "nsnjsd";
+        let watchFood = this.proxies.onFoodChanged(ctx, "todos-list");
+        console.log("PROXY OBJECT", watchFood.todos);
+      })
+    },
+  }
+
+  this.template = html `
+    <div> Hello how are you <button id="change-state"> Change state </button> </div>
+    <div id="todos-list"> </div>
+  `
+}
+
+function isArrayEmpty(arr) {
+  if (arr === undefined || arr.length == 0) {
+    return true;
+  }
+  return false;
+}
+
+let todoList = (todos) => {
+  if (isArrayEmpty(todos) || todos == undefined) {
+    return html `<div> TodoList is empty </div>`
+  } else {
+    return html `<div> We see you at work </div>`
   }
 }
 
-function setExternalState(ctx) {
-  localState = ctx
-  console.log("LOCAL STATE", localState)
+TodoApp.call(unfBaseElement)
 
-}
-
-
-TodoApp.call(UNFBaseElement)
-
-UNF.Base.render("todo-app", UNFBaseElement.ci);
+UNF.Base.render("todo-app", unfBaseElement.ci);
