ad4574b8c2270ba0f47045b24d737a660a6d1972
mar-tina <martinakhaemba@gmail.com>
2020-02-17 14:29:00 +0300

    remove deadcode

--- example/todo-app/src/components/todo.js
+++ example/todo-app/src/components/todo.js
@@ -10,7 +10,7 @@ import {
   buttonObject,
   svgObject,
   todoItem
-} from "./teststyle.js";
+} from "./todoStyle";
 
 async function TodoApp() {
   this.data = {
--- example/todo-app/src/index.js
+++ example/todo-app/src/index.js
@@ -2,15 +2,9 @@ import {
   UNF,
   html
 } from '../../../dist/index.js';
-import "./components/test.js";
-
-let props = {
-  name: "All props"
-};
 
 let newtemplate = html `
   <div>
-      <!-- <test-list props=""> </test-list> -->
       <todo-app> </todo-app>
   </div>
 `;
